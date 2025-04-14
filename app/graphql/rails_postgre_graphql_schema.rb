d5a4cbf02bcb6df133f7cc0b161aab534b1be35d
Abhay Shankur (JOSH) <abhay.shankur@joshsoftware.com>
2025-04-14 16:29:56 +0530

    Minor changes

--- app/graphql/rails_postgre_graphql_schema.rb
+++ app/graphql/rails_postgre_graphql_schema.rb
@@ -3,9 +3,12 @@
 class RailsPostgreGraphqlSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
-
+  
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
   use GraphQL::Dataloader
+  
+  use GraphQL::Subscriptions::ActionCableSubscriptions
+  subscription(Types::SubscriptionType)
 
   # GraphQL-Ruby calls this when something goes wrong while running a query:
   def self.type_error(err, context)
